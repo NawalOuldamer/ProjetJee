f7536ba55354e1a3fcd2e75dfe83dc0254422db1
Nawal.O.A <Nawal.O.A@130.120.242.172>
2013-04-12 14:00:17 +0200

    commit modif des vues et controllers

--- grails-app/domain/projetjee/profile/User.groovy
+++ grails-app/domain/projetjee/profile/User.groovy
@@ -5,19 +5,19 @@ class User {
 	String nom
 	String prenom
 	String mail
-	String pwd
+	String password
 	String role
 	
 	
 	
 	public String toString() {
-		return "Utilisateur [Nom = "+nom+",Prenom = "+prenom+",Mail = "+mail+" ,Mot de passe = "+pwd+"Role = "+role+"]";
+		return "Utilisateur [Nom = "+nom+",Prenom = "+prenom+",Mail = "+mail+" ,Mot de passe = "+password+"Role = "+role+"]";
 	}
     
 	static constraints = {
 		nom nullable:false , blank:false
 		prenom nullable:false, blank:false
-		pwd nullable:false, size:8..10
+		password nullable:false, size:8..10
 		role inList:["Enseignant","Etudiant"]
 		mail email:true, blank:false
 		
